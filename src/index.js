d2afd88df9697ba333616b8cc549e25a82441cd8
Nathan Habtamu <elduhade@gmail.com>
2025-08-09 14:03:57 +0000

    sui

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+// Optionally, you can add some JS code here to interact with your HTML
+
+// Example: Log a message to confirm JS is loaded
+console.log('index.js is loaded');
